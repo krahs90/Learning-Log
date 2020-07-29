96192c3ce9f34bfb7260b050efdb0723bf3eee26
PureNothingness <fg2020husky@gmail.com>
2020-07-29 14:49:52 -0700

    Working app, and styled

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """A topic the user is learning about."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model."""
@@ -20,4 +22,7 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
-        return f"{self.text[:50]}..."
+        if len(self.text) <= 50:
+            return f"{self.text[:50]}"
+        else:
+            return f"{self.text[:50]}..."
